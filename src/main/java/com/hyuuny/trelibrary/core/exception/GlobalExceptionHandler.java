19e4029d39d53c25b8b2d60e47ec0836377e509f
hyuuny <shyune@knou.ac.kr>
2025-08-11 17:29:04 +0900

    feat: GlobalExceptionHandler 추가

---
+++ src/main/java/com/hyuuny/trelibrary/core/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.hyuuny.trelibrary.core.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(NoSuchElementException ex) {
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
