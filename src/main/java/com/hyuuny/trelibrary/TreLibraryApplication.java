80fcbf643bfd134c3e8c79927d149895c5f617f6
hyuuny <shyune@knou.ac.kr>
2025-08-11 14:44:37 +0900

    feat: init project

---
+++ src/main/java/com/hyuuny/trelibrary/TreLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.hyuuny.trelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyuuny/trelibrary/TreLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyuuny.trelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
