e998fb1c5b3ade677ef0139f54583e9c9503ae41
hyuuny <shyune@knou.ac.kr>
2025-08-12 16:22:25 +0900

    test: 도서 단순/복합 검색 기능 테스트 추가

--- src/test/java/com/hyuuny/trelibrary/books/application/BookServiceTest.java
+++ src/test/java/com/hyuuny/trelibrary/books/application/BookServiceTest.java
@@ -1,11 +1,17 @@
 package com.hyuuny.trelibrary.books.application;
 
-import com.hyuuny.trelibrary.BaseTest;
 import com.hyuuny.trelibrary.books.domain.Book;
 import com.hyuuny.trelibrary.books.domain.BookRepository;
+import com.hyuuny.trelibrary.common.BaseTest;
+import com.hyuuny.trelibrary.core.utils.SearchStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.NoSuchElementException;
 
@@ -33,17 +39,17 @@ class BookServiceTest extends BaseTest {
         );
         Book savedBook = bookRepository.save(book);
 
-        BookDto.Response response = bookService.getBook(savedBook.getId());
+        BookDto.DetailResponse response = bookService.getBook(savedBook.getId());
 
-        assertThat(response.getId()).isEqualTo(savedBook.getId());
-        assertThat(response.getTitle()).isEqualTo(savedBook.getTitle());
-        assertThat(response.getSubTitle()).isEqualTo(savedBook.getSubTitle());
-        assertThat(response.getAuthor()).isEqualTo(savedBook.getAuthor());
-        assertThat(response.getPublished()).isEqualTo(savedBook.getPublished());
-        assertThat(response.getIsbn()).isEqualTo(savedBook.getIsbn());
-        assertThat(response.getImageUrl()).isEqualTo(savedBook.getImageUrl());
-        assertThat(response.getCreatedAt()).isEqualTo(savedBook.getCreatedAt());
-        assertThat(response.getLastModifiedAt()).isEqualTo(savedBook.getLastModifiedAt());
+        assertThat(response.id()).isEqualTo(savedBook.getId());
+        assertThat(response.title()).isEqualTo(savedBook.getTitle());
+        assertThat(response.subTitle()).isEqualTo(savedBook.getSubTitle());
+        assertThat(response.author()).isEqualTo(savedBook.getAuthor());
+        assertThat(response.published()).isEqualTo(savedBook.getPublished());
+        assertThat(response.isbn()).isEqualTo(savedBook.getIsbn());
+        assertThat(response.imageUrl()).isEqualTo(savedBook.getImageUrl());
+        assertThat(response.createdAt()).isEqualTo(savedBook.getCreatedAt());
+        assertThat(response.lastModifiedAt()).isEqualTo(savedBook.getLastModifiedAt());
     }
 
     @DisplayName("도서 아이디로 도서를 상세 조회할 때 도서가 없으면 예외가 발생한다")
@@ -54,5 +60,79 @@ class BookServiceTest extends BaseTest {
         assertThatThrownBy(() -> bookService.getBook(nonExistentId))
                 .isInstanceOf(NoSuchElementException.class)
                 .hasMessageContaining("Book notFound id:" + nonExistentId);
+    }
+
+    @DisplayName("title에 keyword가 포함된 도서 목록을 조회할 수 있다")
+    @Test
+    void searchBooksByTitle() {
+        String keyword = "React";
+        Page<BookDto.BookResponse> responses = bookService.searchByKeyword(keyword, PageRequest.of(0, 20));
+        assertThat(responses.getTotalElements()).isEqualTo(7);
+    }
+
+    @DisplayName("title에 keyword가 포함된 도서가 없으면 빈 목록이 조회된다")
+    @Test
+    void searchBooksByTitle_zero() {
+        String keyword = "abc";
+        Page<BookDto.BookResponse> responses = bookService.searchByKeyword(keyword, PageRequest.of(0, 20));
+        assertThat(responses.getTotalElements()).isEqualTo(0);
+    }
+
+    @DisplayName("title 또는 subTitle에 q가 포함된 도서 목록을 조회할 수 있다")
+    @Test
+    void searchComplexQueryBooksByTitleOrSubTitle() {
+        int editionCount = 13;
+        int javaCount = 11;
+        String q = "Edition|Java";
+
+        BookDto.SearchResultDto searchResultDto = bookService.searchByComplexQuery(q, PageRequest.of(0, 20));
+        assertThat(searchResultDto.searchQuery()).isEqualTo(q);
+        assertThat(searchResultDto.pageInfo().currentPage()).isEqualTo(1);
+        assertThat(searchResultDto.pageInfo().pageSize()).isEqualTo(20);
+        assertThat(searchResultDto.pageInfo().totalPages()).isEqualTo(2);
+        assertThat(searchResultDto.pageInfo().totalElements()).isEqualTo(editionCount + javaCount);
+        assertThat(searchResultDto.searchMetadata().strategy()).isEqualTo(SearchStrategy.OR_OPERATION);
+    }
+
+    @DisplayName("title 또는 subTitle에 Edition이 포함되고, Java가 제외된 도서 목록을 조회할 수 있다")
+    @Test
+    void searchComplexQueryBooksByTitleNot() {
+        int editionCount = 13;
+        String q = "Edition-Java";
+
+        BookDto.SearchResultDto searchResultDto = bookService.searchByComplexQuery(q, PageRequest.of(0, 20));
+        assertThat(searchResultDto.searchQuery()).isEqualTo(q);
+        assertThat(searchResultDto.pageInfo().currentPage()).isEqualTo(1);
+        assertThat(searchResultDto.pageInfo().pageSize()).isEqualTo(20);
+        assertThat(searchResultDto.pageInfo().totalPages()).isEqualTo(1);
+        assertThat(searchResultDto.pageInfo().totalElements()).isEqualTo(editionCount);
+        assertThat(searchResultDto.searchMetadata().strategy()).isEqualTo(SearchStrategy.NOT_OPERATION);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("q가 null이거나 빈 값이면 예외가 발생한다")
+    void searchComplexQuery_requireText_ThrowsException(String q) {
+        assertThatThrownBy(() -> bookService.searchByComplexQuery(q, PageRequest.of(0, 20)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("검색어는 필수값 입니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Edition|Java|React", "Edition-Java-React"})
+    @DisplayName("검색어에 | 또는 - 가 하나 이상이면 예외가 발생한다")
+    void searchComplexQuery_over_ThrowsException(String q) {
+        assertThatThrownBy(() -> bookService.searchByComplexQuery(q, PageRequest.of(0, 20)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("검색어는 '|' 또는 '-' 연산자를 각각 1개만 포함할 수 있습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Edition-Java|React", "Edition|Java-React"})
+    @DisplayName("검색어에 |와 - 가 같이 사용되면 예외가 발생한다")
+    void searchComplexQuery_with_ThrowsException(String q) {
+        assertThatThrownBy(() -> bookService.searchByComplexQuery(q, PageRequest.of(0, 20)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("검색어는 '|' 와 '-' 연산자를 동시에 사용할 수 없습니다.");
     }
 }
--- src/test/java/com/hyuuny/trelibrary/common/BaseTest.java
+++ src/test/java/com/hyuuny/trelibrary/common/BaseTest.java
@@ -1,11 +1,13 @@
-package com.hyuuny.trelibrary;
+package com.hyuuny.trelibrary.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+@Import(TestDataInitializer.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
